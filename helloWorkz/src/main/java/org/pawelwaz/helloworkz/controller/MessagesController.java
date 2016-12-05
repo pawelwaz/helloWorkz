a6b6285fc2de541c53d95eae4c31fc1621332ade
pawelwaz <pawelwazit@gmail.com>
2016-12-05 18:36:20 +0100

    msg archieve

--- helloWorkz/src/main/java/org/pawelwaz/helloworkz/controller/MessagesController.java
+++ helloWorkz/src/main/java/org/pawelwaz/helloworkz/controller/MessagesController.java
@@ -1,22 +1,28 @@
 package org.pawelwaz.helloworkz.controller;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import org.pawelwaz.helloworkz.entity.HelloUser;
+import org.pawelwaz.helloworkz.entity.Message;
 import org.pawelwaz.helloworkz.util.HelloSession;
 import org.pawelwaz.helloworkz.util.HelloUI;
 import org.pawelwaz.helloworkz.util.JpaUtil;
@@ -27,8 +33,9 @@ import org.pawelwaz.helloworkz.util.JpaUtil;
  */
 public class MessagesController extends HelloUI {
     
-    @FXML VBox leftVB;
-    @FXML VBox rightVB;
+    @FXML private VBox leftVB;
+    @FXML private WebView rightView;
+    private HelloUser msgPerson = null;
     
     private List<HelloUser> getConversations() {
         List<HelloUser> persons = new ArrayList();
@@ -55,30 +62,72 @@ public class MessagesController extends HelloUI {
         return persons;
     }
     
+    private void showMessages() {
+        EntityManager em = JpaUtil.getFactory().createEntityManager();
+        Query q = em.createQuery("select m from Message m where (m.sender = :user1 and m.receiver = :user2) or (m.sender = :user2 and m.receiver = :user1) order by m.sendTime desc");
+        q.setParameter("user1", HelloSession.getUser().getId());
+        q.setParameter("user2", this.msgPerson.getId());
+        q.setMaxResults(30);
+        List<Message> messages = q.getResultList();
+        int n = 0;
+        StringBuilder output = new StringBuilder("");
+        for(Message msg : messages) {
+            String color = "#BFCCCE";
+            if(n % 2 == 1) color = "#CAD5D7";
+            HelloUser user;
+            if(Objects.equals(msg.getSender(), HelloSession.getUser().getId())) user = HelloSession.getUser();
+            else user = this.msgPerson;
+            output.insert(0, HelloUI.prepareMessageStripe(user, msg.getContent(), new SimpleDateFormat("yyyy-MM-dd HH:mm").format(msg.getSendTime()), color, HelloSession.getHtmlAvatar()));
+            n++;
+        }
+        output.insert(0, "<body style=\"font-family: System; margin: 0px;\"><table cellpadding=\"5\" cellspacing=\"0\" style=\"font-size: 12px; width: 100%; border: 0px;\">");
+        output.append("</table>");
+        output.append("</body>");
+        this.rightView.getEngine().loadContent(output.toString());
+        em.close();
+    }
+    
     private void showConversations() {
         List<HelloUser> persons = this.getConversations();
         this.leftVB.getChildren().clear();
         Label header = new Label("lista rozmów");
         header.getStyleClass().add("blueLabelSm");
         this.leftVB.getChildren().add(header);
-        int n = 0;
-        for(HelloUser person : persons) {
-            HBox row = new HBox();
-            if(n % 2 == 0) row.getStyleClass().add("stripeEven");
-            else row.getStyleClass().add("stripeOdd");
-            row.setCursor(Cursor.HAND);
-            row.setPadding(new Insets(5, 5, 5, 5));
-            person.prepareAvatar();
-            ImageView userAvatar = new ImageView();
-            userAvatar.setImage(SwingFXUtils.toFXImage(person.getReadyAvatar(), null));
-            userAvatar.setFitWidth(60.0);
-            userAvatar.setFitHeight(60.0);
-            row.getChildren().add(userAvatar);
-            Label userName = new Label(person.getLogin());
-            userName.getStyleClass().add("smallHeader");
-            row.getChildren().add(userName);
-            this.leftVB.getChildren().add(row);
-            n++;
+        if(persons.isEmpty()) {
+            Label noResults = new Label("(Brak rozmów do wyświetlenia)");
+            noResults.getStyleClass().add("blueLabelSm");
+            this.leftVB.getChildren().add(noResults);
+        }
+        else {
+            int n = 0;
+            for(HelloUser person : persons) {
+                HBox row = new HBox();
+                if(n % 2 == 0) row.getStyleClass().add("stripeEven");
+                else row.getStyleClass().add("stripeOdd");
+                row.setCursor(Cursor.HAND);
+                row.setPadding(new Insets(5, 5, 5, 5));
+                person.prepareAvatar();
+                ImageView userAvatar = new ImageView();
+                userAvatar.setImage(SwingFXUtils.toFXImage(person.getReadyAvatar(), null));
+                userAvatar.setFitWidth(60.0);
+                userAvatar.setFitHeight(60.0);
+                row.getChildren().add(userAvatar);
+                Label userName = new Label(person.getLogin());
+                userName.getStyleClass().add("smallHeader");
+                row.getChildren().add(userName);
+                row.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        showMessages();
+                        rightView.getEngine().executeScript("window.scrollTo(0, document.body.scrollHeight)");
+                    }
+                });
+                this.leftVB.getChildren().add(row);
+                n++;
+            }
+            if(this.msgPerson == null) {
+                this.msgPerson = persons.get(0);
+            }
         }
     }
     
