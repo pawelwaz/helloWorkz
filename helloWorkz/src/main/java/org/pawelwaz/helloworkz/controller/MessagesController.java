226820a7ecfca1a9cefab19ed7b75e44f5aa33c2
pawelwaz <pawelwazit@gmail.com>
2016-12-07 19:35:57 +0100

    msg archieve

--- helloWorkz/src/main/java/org/pawelwaz/helloworkz/controller/MessagesController.java
+++ helloWorkz/src/main/java/org/pawelwaz/helloworkz/controller/MessagesController.java
@@ -8,7 +8,10 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -16,6 +19,7 @@ import javafx.scene.Cursor;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
@@ -36,6 +40,8 @@ public class MessagesController extends HelloUI {
     @FXML private VBox leftVB;
     @FXML private WebView rightView;
     private HelloUser msgPerson = null;
+    private StringBuilder messagesHtml = new StringBuilder("");
+    private int msgCount = 0;
     
     private List<HelloUser> getConversations() {
         List<HelloUser> persons = new ArrayList();
@@ -62,24 +68,30 @@ public class MessagesController extends HelloUI {
         return persons;
     }
     
+    private void loadMessages() {
+        
+    }
+    
     private void showMessages() {
         EntityManager em = JpaUtil.getFactory().createEntityManager();
         Query q = em.createQuery("select m from Message m where (m.sender = :user1 and m.receiver = :user2) or (m.sender = :user2 and m.receiver = :user1) order by m.sendTime desc");
         q.setParameter("user1", HelloSession.getUser().getId());
         q.setParameter("user2", this.msgPerson.getId());
+        q.setFirstResult(msgCount);
         q.setMaxResults(30);
         List<Message> messages = q.getResultList();
+        msgCount += messages.size();
         int n = 0;
-        StringBuilder output = new StringBuilder("");
         for(Message msg : messages) {
             String color = "#BFCCCE";
             if(n % 2 == 1) color = "#CAD5D7";
             HelloUser user;
             if(Objects.equals(msg.getSender(), HelloSession.getUser().getId())) user = HelloSession.getUser();
             else user = this.msgPerson;
-            output.insert(0, HelloUI.prepareMessageStripe(user, msg.getContent(), new SimpleDateFormat("yyyy-MM-dd HH:mm").format(msg.getSendTime()), color, HelloSession.getHtmlAvatar()));
+            messagesHtml.insert(0, HelloUI.prepareMessageStripe(user, msg.getContent(), new SimpleDateFormat("yyyy-MM-dd HH:mm").format(msg.getSendTime()), color, HelloSession.getHtmlAvatar()));
             n++;
         }
+        StringBuilder output = new StringBuilder(messagesHtml.toString());
         output.insert(0, "<body style=\"font-family: System; margin: 0px;\"><table cellpadding=\"5\" cellspacing=\"0\" style=\"font-size: 12px; width: 100%; border: 0px;\">");
         output.append("</table>");
         output.append("</body>");
@@ -119,7 +131,6 @@ public class MessagesController extends HelloUI {
                     @Override
                     public void handle(MouseEvent event) {
                         showMessages();
-                        rightView.getEngine().executeScript("window.scrollTo(0, document.body.scrollHeight)");
                     }
                 });
                 this.leftVB.getChildren().add(row);
@@ -129,10 +140,17 @@ public class MessagesController extends HelloUI {
                 this.msgPerson = persons.get(0);
             }
         }
+        this.rightView.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if(msgCount <= 30) rightView.getEngine().executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            }
+        });
     }
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        super.initialize(location, resources);
         this.showConversations();
     }
     
