166804530b93f373ace86ef12823eb1475b4e659
pawelwaz <pawelwazit@gmail.com>
2017-02-02 17:14:38 +0100

    host address

--- helloWorkz/src/main/java/org/pawelwaz/helloworkz/util/JpaUtil.java
+++ helloWorkz/src/main/java/org/pawelwaz/helloworkz/util/JpaUtil.java
@@ -1,5 +1,10 @@
 package org.pawelwaz.helloworkz.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -12,7 +17,12 @@ public class JpaUtil {
     
     public static void buildFactory() {
         try {
-            factory = Persistence.createEntityManagerFactory("hello");
+            FileReader fileReader = new FileReader(new File("host.txt"));
+            BufferedReader br = new BufferedReader(fileReader);
+            String host = br.readLine();
+            Map<String, String> persistenceMap = new HashMap<String, String>();
+            persistenceMap.put("javax.persistence.jdbc.url", "jdbc:mysql://" + host + ":3306/hello?useUnicode=yes&amp;characterEncoding=UTF-8");
+            factory = Persistence.createEntityManagerFactory("hello", persistenceMap);
         }
         catch(Exception ex) {
             HelloUI.showError("Wystąpił błąd połączenia z bazą danych. Aplikacja zostanie zamknięta");
